f730645052c60c68e9500888044c0f5a4efd9d73
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-12-25 13:20:29 +0200

    error in assigning a variable fixed

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,7 @@ const tokyo = {
   link: "https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=2995&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
-initialCards = [
+const initialCards = [
   acores,
   deadSea,
   pamukkale,
