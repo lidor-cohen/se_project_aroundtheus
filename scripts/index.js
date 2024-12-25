023bd618d163e20a4bd1e339a1416fd8d5df1e2a
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-12-25 13:15:21 +0200

    changed images structure and added first js file

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+const acores = {
+  name: "acores",
+  link: "../images/places/acores.jpg",
+};
+
+const deadSea = {
+  name: "acores",
+  link: "../images/places/acores.jpg",
+};
+
+const pamukkale = {
+  name: "acores",
+  link: "../images/places/acores.jpg",
+};
+
+const salarDeUyuni = {
+  name: "acores",
+  link: "../images/places/acores.jpg",
+};
+
+const theGiantsCauseway = {
+  name: "acores",
+  link: "../images/places/acores.jpg",
+};
+
+const tokyo = {
+  name: "acores",
+  link: "../images/places/acores.jpg",
+};
+
+initialCards = [
+  acores,
+  deadSea,
+  pamukkale,
+  salarDeUyuni,
+  theGiantsCauseway,
+  tokyo,
+];
