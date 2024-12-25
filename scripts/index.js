f797f0aa8745ab6c74f420d7ec49e1b8dc2d9154
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-12-25 13:48:47 +0200

    changed images to tripleten content

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,26 @@
-const acores = {
-  name: "Acores",
-  link: "https://images.unsplash.com/photo-1629201907986-355596a3e3ab?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const deadSea = {
-  name: "Dead Sea",
-  link: "https://images.unsplash.com/photo-1726001739725-cfd1902b2a2b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const pamukkale = {
-  name: "Pamukkale",
-  link: "https://images.unsplash.com/photo-1728466698701-2eb2af4117d4?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const salarDeUyuni = {
-  name: "Salar de Uyuni",
-  link: "https://images.unsplash.com/photo-1730462826088-ef07f32b2629?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const theGiantsCauseway = {
-  name: "The Giant's Causeway",
-  link: "https://images.unsplash.com/photo-1569531412913-08101790fb10?q=80&w=2805&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const tokyo = {
-  name: "Tokyo",
-  link: "https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=2995&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
 const initialCards = [
-  acores,
-  deadSea,
-  pamukkale,
-  salarDeUyuni,
-  theGiantsCauseway,
-  tokyo,
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
