4ffc5668908549f3bbcdf96bdfeb66dfa88029d3
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-12-25 13:19:47 +0200

    changed images to limks

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 const acores = {
-  name: "acores",
-  link: "../images/places/acores.jpg",
+  name: "Acores",
+  link: "https://images.unsplash.com/photo-1629201907986-355596a3e3ab?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 const deadSea = {
-  name: "acores",
-  link: "../images/places/acores.jpg",
+  name: "Dead Sea",
+  link: "https://images.unsplash.com/photo-1726001739725-cfd1902b2a2b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 const pamukkale = {
-  name: "acores",
-  link: "../images/places/acores.jpg",
+  name: "Pamukkale",
+  link: "https://images.unsplash.com/photo-1728466698701-2eb2af4117d4?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 const salarDeUyuni = {
-  name: "acores",
-  link: "../images/places/acores.jpg",
+  name: "Salar de Uyuni",
+  link: "https://images.unsplash.com/photo-1730462826088-ef07f32b2629?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 const theGiantsCauseway = {
-  name: "acores",
-  link: "../images/places/acores.jpg",
+  name: "The Giant's Causeway",
+  link: "https://images.unsplash.com/photo-1569531412913-08101790fb10?q=80&w=2805&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 const tokyo = {
-  name: "acores",
-  link: "../images/places/acores.jpg",
+  name: "Tokyo",
+  link: "https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=2995&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 initialCards = [
