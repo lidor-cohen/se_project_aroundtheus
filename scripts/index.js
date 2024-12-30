1636847aa4a278841fa03cb1d834e8275eb50353
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-12-30 17:40:56 +0200

    Added edit profile logic and dynamic gallery items with js

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,53 @@ const initialCards = [
   },
 ];
 
+function toggleModalVisibility() {
+  editProfileModal.classList.toggle("modal_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = modalNameInput.value;
+  profileJob.textContent = modalJobInput.value;
+  editProfileModal.classList.toggle("modal_opened");
+}
+
+function getCardElement(card) {
+  const cardTemplate = document.querySelector("#card-template").content;
+
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__name");
+
+  cardImage.alt = card.name;
+  cardImage.src = card.link;
+  cardTitle.textContent = card.name;
+
+  return cardElement;
+}
+
 const editProfileButton = document.querySelector(".profile__edit-name-button");
+
+const profileInfo = document.querySelector(".profile__info");
+const profileName = profileInfo.querySelector(".profile__name");
+const profileJob = profileInfo.querySelector(".profile__job");
+
 const editProfileModal = document.querySelector(".modal");
 const modalExitButton = editProfileModal.querySelector(".modal__exit-btn");
+const modalNameInput = editProfileModal.querySelector("#name");
+const modalJobInput = editProfileModal.querySelector("#job");
+const modalForm = editProfileModal.querySelector(".modal__form");
 
-editProfileButton.addEventListener("click", function (event) {
-  editProfileModal.classList.toggle("modal_opened");
-});
+modalNameInput.value = profileName.textContent;
+modalJobInput.value = profileJob.textContent;
 
-modalExitButton.addEventListener("click", function (event) {
-  editProfileModal.classList.toggle("modal_opened");
-});
+editProfileButton.addEventListener("click", toggleModalVisibility);
+modalExitButton.addEventListener("click", toggleModalVisibility);
+modalForm.addEventListener("submit", handleProfileFormSubmit);
+
+const gallery = document.querySelector(".gallery");
+let changingCardElement = null;
+for (item of initialCards) {
+  changingCardElement = getCardElement(item);
+  gallery.append(changingCardElement);
+}
